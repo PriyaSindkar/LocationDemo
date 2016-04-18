04bb51bd13f7c47d30edc9840cd938824c66b60f
priyasindkar <sindkar.priya@gmail.com>
2016-04-18 12:57:31 +0530

    nkmk

--- app/src/main/java/com/example/priyasindkar/locationdemo/MapActivity.java
+++ app/src/main/java/com/example/priyasindkar/locationdemo/MapActivity.java
@@ -5,11 +5,9 @@ package com.example.priyasindkar.locationdemo;
  */
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -34,25 +32,17 @@ public class MapActivity extends FragmentActivity implements
 
     public static final String TAG = MapActivity.class.getSimpleName();
 
-    /*
-     * Define a request code to send to Google Play services
-     * This code is returned in Activity.onActivityResult
-     */
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
-    // private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
-    double currentLatitude, currentLongitude;
     private TextView text;
-    private boolean isFirstTime = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        //setUpMapIfNeeded();
+
+        text = (TextView) findViewById(R.id.text);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -63,143 +53,71 @@ public class MapActivity extends FragmentActivity implements
         // Create the LocationRequest object
         mLocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setInterval(10 * 1000)
                 .setFastestInterval(1 * 1000)
-                /*.setNumUpdates(1)*/;
-
-        mGoogleApiClient.connect();
-
-        text = (TextView) findViewById(R.id.text);
+                .setNumUpdates(1);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //setUpMapIfNeeded();
-
-        if (mGoogleApiClient.isConnected()) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    mGoogleApiClient, mLocationRequest, this);
-        }
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
         if (mGoogleApiClient.isConnected()) {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
             mGoogleApiClient.disconnect();
         }
     }
 
-    /*
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }*/
-
-   /* */
-
-    /*
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }*/
     private void handleNewLocation(Location location) {
-        Log.e("handleNewLocation", "handleNewLocation");
-       /* if(!isFirstTime) {
-            if (currentLatitude != location.getLatitude() && currentLongitude != location.getLongitude()) {
-                text.setText("Latitude: " + currentLatitude + "\n" + "Longitude: " + currentLongitude);
-            }
-        } else {
-            isFirstTime = false;
-            text.setText("Latitude: " + currentLatitude + "\n" + "Longitude: " + currentLongitude);
-        }*/
+        Log.e(TAG, location.toString());
 
-        currentLatitude = location.getLatitude();
-        currentLongitude = location.getLongitude();
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
         LatLng latLng = new LatLng(currentLatitude, currentLongitude);
-        text.setText("Latitude: " + currentLatitude + "\n" + "Longitude: " + currentLongitude);
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
-        /*MarkerOptions options = new MarkerOptions()
-                .position(latLng)
-                .title("I am here!");
-        mMap.addMarker(options);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));*/
+        text.setText("Latitude: " + currentLatitude + "\nLongitude: " + currentLongitude);
     }
 
     @Override
     public void onConnected(Bundle bundle) {
-        Log.e("onConnected", "onConnected");
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
+        Log.e(TAG, "onConnected");
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         boolean gps_enabled = false;
         try {
             gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
         } catch (Exception ex) {
+            Log.e("GPS EX", ex.toString());
         }
 
         if (!gps_enabled) {
             showSettingsAlert();
         } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
             Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
             if (location == null) {
-                Log.e("location", "null");
                 LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
             } else {
                 handleNewLocation(location);
             }
         }
-
-
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
+
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
-        }
+        Log.e(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
     }
 
     @Override
@@ -208,12 +126,11 @@ public class MapActivity extends FragmentActivity implements
         handleNewLocation(location);
     }
 
-
     public void showSettingsAlert() {
         final LatLng[] latLng = {null};
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        android.app.AlertDialog.Builder alertDialog = new android.app.AlertDialog.Builder(this);
         alertDialog.setTitle("Location Settings");
-        alertDialog.setMessage("Unable to Track Current Latitude and Longitude. Click Yes To Use Your Current Location For Better Search.");
+        alertDialog.setMessage("Unable to Track Current      Latitude and Longitude. Click Yes To Use Your Current Location For Better Search.");
         alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
